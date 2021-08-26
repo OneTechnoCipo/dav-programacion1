26796cf32e16297d81b4b6b8ddf667f52a5c927c
OneTechnoCipo <marianocollazo2003@gmai.com>
2021-08-26 17:04:04 -0300

    Bhaskara

---
+++ ejercicio2/main.js
@@ -0,0 +1,19 @@
+/* function calcular (x){
+    let dato =10;
+    return x;
+} */
+
+const calcular=()=>{ 
+    const datoa = document.getElementById("inp_a").value; /* (const) constante no varia el dato y (let) si el dato solo vive en un proceso */
+    const datob = document.getElementById("inp_b").value; /* (const) constante no varia el dato y (let) si el dato solo vive en un proceso */
+    const datoc = document.getElementById("inp_c").value; /* (const) constante no varia el dato y (let) si el dato solo vive en un proceso */
+    const parte1 = (Math.pow(datob,2))-(4*datoa*datoc);/* dato 1 (Cateto opuesto)elevado al cuadrado */
+    const raíz = Math.sqrt(parte1);
+
+    const r1=(-datob+raíz)/ (2*datoa);
+    const r2=(-datob-raíz)/ (2*datoa);
+    document.getElementById("r1").textContent= 'el resultado de raíz 1 es: '+r1;
+    document.getElementById("r2").textContent= 'el resultado de raíz 2 es: '+r2;
+}
+const boton=document.getElementById("btn-calcular");
+boton.addEventListener("click",calcular);/* click al dar un click izquierdo del mouse, dbclick doble click izquierdo */
