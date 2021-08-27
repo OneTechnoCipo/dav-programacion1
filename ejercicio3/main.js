4cc76aca3cc1a853e68914e59da0ae730d6c4cde
OneTechnoCipo <marianocollazo2003@gmai.com>
2021-08-27 15:49:28 -0300

    AREA DE UN CIRCULO

---
+++ ejercicio3/main.js
@@ -0,0 +1,9 @@
+/* TP 3 */
+const calcular=()=>{ 
+    const datoa = document.getElementById("inp_a").value; /* (const) constante no varia el dato y (let) si el dato solo vive en un proceso */
+    const parte1 = (Math.pow (datoa,2)*(Math.PI));/*  */
+
+    document.getElementById("parte1").textContent= 'el resultado de area es: '+parte1;
+}
+const boton=document.getElementById("btn-calcular");
+boton.addEventListener("click",calcular);/* click al dar un click izquierdo del mouse, dbclick doble click izquierdo */
