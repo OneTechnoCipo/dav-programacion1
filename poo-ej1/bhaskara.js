faf3172329f49ac8f9005527bfb1971e8165ba71
OneTechnoCipo <marianocollazo2003@gmai.com>
2021-09-02 16:55:25 -0300

    Bhaskara

--- poo-ej1/bhaskara.js
+++ poo-ej1/bhaskara.js
@@ -13,12 +13,12 @@ export default class Bhaskara{
         const bloque1 = (Math.pow(this.cb,2))-(4*this.ca*this.ti);/* dato 1 (Cateto opuesto)elevado al cuadrado */
         const raíz = Math.sqrt(bloque1);
         const r1=(-this.cb+raíz)/ (2*this.ca);
-        return r1;
+        return r1.toFixed(2);
     }
     calcular_r2(){
         const bloque1 = (Math.pow(this.cb,2))-(4*this.ca*this.ti);/* dato 1 (Cateto opuesto)elevado al cuadrado */
         const raíz = Math.sqrt(bloque1);
         const r2=(-this.cb-raíz)/ (2*this.ca);
-        return r2;
+        return r2.toFixed(2);
     }
 }
