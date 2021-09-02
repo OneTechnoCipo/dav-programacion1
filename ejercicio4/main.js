9de4a60855c8c0dfbc4d0cca61b2215119d307a7
OneTechnoCipo <marianocollazo2003@gmai.com>
2021-09-02 16:51:50 -0300

    Caída libre

--- ejercicio4/main.js
+++ ejercicio4/main.js
@@ -1,4 +1,4 @@
-/* TP 3 */
+/* TP3 */
 const calcular_tiempo=()=>{ 
     const inpvi = document.getElementById("inp_vi").value; /* (const) constante no varia el dato y (let) si el dato solo vive en un proceso */
     const inpvf = document.getElementById("inp_vf").value; /* (const) constante no varia el dato y (let) si el dato solo vive en un proceso */
